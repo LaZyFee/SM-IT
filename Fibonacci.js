042452270239255ab0158d0c45fe11ed01dab5d8
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-04-11 01:17:15 +0600

    Fibonacci code updated

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,7 +1,15 @@
 //  Write a function to generate the Fibonacci sequence up to a given number of terms.
-const createFibonacci = (number) => {
-    for (i = number; i >= 0; i--) {
-        console.log(i);
+const createFibonacci = (n) => {
+    const sequence = [];
+
+    if (n > 0) sequence[0] = 0;
+    if (n > 1) sequence[1] = 1;
+
+    for (let i = 2; i < n; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
     }
+
+    return sequence;
 }
-createFibonacci(5)
+
+console.log(createFibonacci(10));
